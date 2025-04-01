0450642b10c74198814c074ca8c010e0ceb93ef3
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-04-01 23:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (90.29%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,31 +1,21 @@
-class Solution {
-    public int hIndex(int[] c) {
-        int start=0;
-        int end=max(c);
-        int ans=1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(dec(mid,c)){
-                start=mid+1;
-                ans=mid;
-            }
-            else end=mid-1;
+    class Solution {
+        public int hIndex(int[] citations) {
+            return solve(citations);
         }
-        return ans;
-    }
-    public int max(int[] arr){
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++){
-            max=Math.max(max,arr[i]);
+
+    private int solve(int[] arr) {
+        int n = arr.length;
+        int l = 0;
+        int r = n;
+
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[m] + m >= n)
+                r = m;
+            else
+                l = m + 1;
         }
-        return max;
-    }
-    public boolean dec(int mid,int[] arr){
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(mid<=arr[i]) count++;
-        }
-        if(count>=mid) return true;
-        return false;
+
+        return n - l;
     }
 }
