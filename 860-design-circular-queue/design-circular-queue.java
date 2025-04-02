2987d2d9a88a9b2331078b2c630d48174130a349
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-04-02 10:23:00 +0530

    Time: 4 ms (100.00%) | Memory: 44.8 MB (47.85%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.java
+++ 860-design-circular-queue/design-circular-queue.java
@@ -1,42 +1,63 @@
 class MyCircularQueue {
-  public MyCircularQueue(int k) {
-    this.k = k;
-    this.q = new int[k];
-    this.rear = k - 1;
-  }
-  public boolean enQueue(int value) {
-    if (isFull())
-      return false;
+    int[] queue;
+    int size;
+    int front;
+    int capacity;
 
-    rear = ++rear % k;
-    q[rear] = value;
-    ++size;
-    return true;
-  }
-  public boolean deQueue() {
-    if (isEmpty())
-      return false;
+    public MyCircularQueue(int k) {
+        this.queue = new int[k];
+        this.size = 0;
+        this.capacity = k;
+        this.front = 0;
+    }
 
-    front = ++front % k;
-    --size;
-    return true;
-  }
-  public int Front() {
-    return isEmpty() ? -1 : q[front];
-  }
-  public int Rear() {
-    return isEmpty() ? -1 : q[rear];
-  }
-  public boolean isEmpty() {
-    return size == 0;
-  }
-  public boolean isFull() {
-    return size == k;
-  }
+    public boolean enQueue(int value) {
+        if (isFull())
+            return false;
+        int index = (front + size) % capacity;
+        queue[index] = value;
+        size++;
+        return true;
+    }
 
-  private final int k;
-  private int[] q;
-  private int size = 0;
-  private int front = 0;
-  private int rear;
+    public boolean deQueue() {
+        if (isEmpty())
+            return false;
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    public int Front() {
+         if (isEmpty())
+            return -1;
+        return queue[front];
+    }
+
+    public int Rear() {
+        if (isEmpty())
+            return -1;
+        int index = (front + size - 1) % capacity;
+        return queue[index];
+
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+      return size == capacity;
+    }
 }
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
