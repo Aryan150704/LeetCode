9514c4ccf6643d95c59370e28c3bf34a79ebcf97
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-05-01 22:14:00 +0530

    Time: 3 ms (100.00%) | Memory: 55.8 MB (97.93%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for(int x : nums) {
+            if(max < x) max = x;
+        }
+        int[] f = new int[max + 1];
+        for(int x : nums) {
+            f[x]++;
+        }
+        int ans = 0;
+        for(int i = 0; i < max; i++) {
+            if(f[i] > 1) {
+                f[i+1] += f[i] - 1;
+                ans += f[i] - 1;
+            }
+        }
+        ans += (f[max] - 1) * f[max] / 2;
+        return ans;
+    }
+}
