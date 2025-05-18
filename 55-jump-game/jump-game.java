9f5bb984ff656f74461170816cff8c10b1326acc
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-05-18 09:54:06 +0530

    Time: 1588 ms (5.01%) | Memory: 45.7 MB (70.58%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n=nums.length;
+        boolean[] dp = new boolean[n]; 
+        java.util.Arrays.fill(dp, false);
+        
+        dp[n-1]=true;
+            for(int i=n-2;i>=0;i--){
+                for(int j=1;j<=nums[i];j++){
+                if(i+j<n && dp[i+j]){
+                    dp[i]=true;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
