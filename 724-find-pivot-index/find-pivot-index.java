ed00bd6621f4a6ab5042be0a5178ff28ccac27c9
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-07-11 13:31:26 +0530

    Time: 1 ms (92.84%) | Memory: 45.9 MB (26.46%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,18 +1,26 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int[] pre=new int[nums.length];
-        int[] suf=new int[nums.length];
-        pre[0]=nums[0];
+        if(nums.length==1)return 0;
+        int[] prefix=new int[nums.length];
+        int[] suffix=new int[nums.length];
+        prefix[0]=nums[0];
+        suffix[nums.length-1]=nums[nums.length-1];
         for(int i=1;i<nums.length;i++){
-            pre[i]=nums[i]+pre[i-1];
+            prefix[i]=nums[i]+prefix[i-1];
         }
-        suf[nums.length-1]=nums[nums.length-1];
         for(int i=nums.length-2;i>=0;i--){
-            suf[i]=suf[i+1]+nums[i];
+            suffix[i]=suffix[i+1]+nums[i];
         }
         for(int i=0;i<nums.length;i++){
-            if(suf[i]==pre[i]) return i;
+            if(i==0 || i==nums.length-1){
+                if(i==0 && suffix[i+1]==0) return i;
+                else if(i==nums.length-1 && prefix[i-1]==0) return i;
+            }
+            else{
+                if(prefix[i-1]==suffix[i+1]) return i;
+            }
         }
+        
         return -1;
     }
 }
