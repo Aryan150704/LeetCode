a2db58c257a1ecea225db0181c5e45483eb0dd42
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-07-26 16:06:47 +0530

    Time: 1 ms (92.61%) | Memory: 46 MB (18.78%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,26 +1,27 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        if(nums.length==1)return 0;
-        int[] prefix=new int[nums.length];
-        int[] suffix=new int[nums.length];
+        if(nums.length==1) return 0;
+        int n=nums.length;
+        int prefix[]=new int[n];
+        int suffix[]=new int[n];
         prefix[0]=nums[0];
-        suffix[nums.length-1]=nums[nums.length-1];
-        for(int i=1;i<nums.length;i++){
+        suffix[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
             prefix[i]=nums[i]+prefix[i-1];
         }
-        for(int i=nums.length-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
             suffix[i]=suffix[i+1]+nums[i];
         }
-        for(int i=0;i<nums.length;i++){
-            if(i==0 || i==nums.length-1){
-                if(i==0 && suffix[i+1]==0) return i;
-                else if(i==nums.length-1 && prefix[i-1]==0) return i;
+        for(int i=0;i<n;i++){
+            if(i==0 || i==n-1){
+                if(i==0 && suffix[i+1]==0) return 0;
+                else if( i==n-1 && prefix[n-2]==0) return n-1;
             }
             else{
-                if(prefix[i-1]==suffix[i+1]) return i;
+                if(prefix[i]==suffix[i]) return i;
             }
         }
-        
         return -1;
     }
 }
+
