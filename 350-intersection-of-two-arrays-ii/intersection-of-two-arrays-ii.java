2606a5151746a7d85ee86ecef3f3163d892ad664
Aryan <160862593+Aryan150704@users.noreply.github.com>
2025-11-12 17:28:52 +0530

    Time: 3 ms (91.10%) | Memory: 45.3 MB (7.42%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,37 +1,23 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> hp1=new HashMap<>();
-        HashMap<Integer,Integer> hp2=new HashMap<>();
-        HashSet<Integer> h1=new HashSet<>();
-        HashSet<Integer> h2=new HashSet<>();
-        ArrayList<Integer> arr=new ArrayList<>();
+        HashMap<Integer,Integer> h1=new HashMap<>();
         for(int num:nums1){
-            if(!hp1.containsKey(num)){
-                hp1.put(num,0);
+            if(!h1.containsKey(num)){
+                h1.put(num,0);
             }
-            hp1.put(num,hp1.get(num)+1);
-            if(!h1.contains(num))h1.add(num);
+            h1.put(num,h1.get(num)+1);
         }
+        ArrayList<Integer> arr=new ArrayList<>();
         for(int num:nums2){
-            if(!hp2.containsKey(num)){
-                hp2.put(num,0);
-            }
-            hp2.put(num,hp2.get(num)+1);
-            if(!h2.contains(num)){
-                h2.add(num);
-            }
-            
-        }
-        h1.retainAll(h2);
-        for(int num:h1){
-            int count=Math.min(hp1.get(num),hp2.get(num));
-            while(count>0){
-                arr.add(num);
-                count--;
+            if(h1.containsKey(num)){
+                if(h1.get(num)>0) {
+                    h1.put(num,h1.get(num)-1);
+                    arr.add(num);
+                }
             }
         }
         int[] ans=new int[arr.size()];
-        for(int i=0;i<ans.length;i++){
+        for(int i=0;i<arr.size();i++){
             ans[i]=arr.get(i);
         }
         return ans;
