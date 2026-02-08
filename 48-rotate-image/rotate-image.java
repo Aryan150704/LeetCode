106027570ed7b3b3ee1a1ffda0dba37695d2e43e
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-02-09 05:20:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (49.25%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,17 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int[][] arr=new int[matrix.length][matrix[0].length];
-        int l=matrix[0].length-1;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                arr[j][l]=matrix[i][j];
-            }
-            l--;
-        }
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                matrix[i][j]=arr[i][j];
+        for(int i=0;i<matrix.length-1;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                swap(matrix,i,j);
             }
         }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][matrix[0].length-j-1];
+                matrix[i][matrix[0].length-j-1]=temp;
+            }
+        }
+    }
+    public void swap(int[][] arr,int i,int j){
+        int temp=arr[i][j];
+        arr[i][j]=arr[j][i];
+        arr[j][i]=temp;
     }
 }
