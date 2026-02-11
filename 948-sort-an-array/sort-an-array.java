b181fec398ca971d966dc17dbed30ea84de1fe24
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-02-11 05:35:46 +0530

    Time: 27 ms (53.61%) | Memory: 87 MB (8.88%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,43 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+        mergSort(nums,0,nums.length-1);
         return nums;
+    }
+    public void mergSort(int[] arr,int si,int ei){
+        if(si>=ei){
+            return;
+        } 
+        int mid=si+(ei-si)/2;
+        mergSort(arr,si,mid); // left part
+        mergSort(arr,mid+1,ei); // right part
+        merge(arr,si,mid,ei);
+    }
+    public void merge(int[] arr,int si,int mid,int ei){
+        int i=si;
+        int j=mid+1;
+        int temp[]=new int[ei-si+1];
+        int k=0;
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        while(j<=ei){
+            temp[k++]=arr[j++];
+        }
+        int n=0;
+        for(int m=si;m<=ei;m++){
+            arr[m]=temp[n];
+            n++;
+        }
     }
 }
