acf0d6dbcc036de55f6fcfb309a445a397a7eead
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-02-25 16:17:04 +0530

    Time: 12 ms (2.77%) | Memory: 45.7 MB (11.05%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,29 @@
 class Solution {
-    public boolean search(int[] arr, int target) {
-        int str=0;
-        int end=arr.length-1;
-        while(str<=end){
-            
-            int mid=str+(end-str)/2;
-            if(arr[mid]==target){
-                return true;
-            }
-            if(arr[str]==arr[mid] && arr[mid]==arr[end]){
-                str++;
+    public boolean search(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            System.out.println(start+" "+ mid+" "+end);
+            if(nums[mid]==target) return true;
+            if(nums[start]==nums[mid]&& nums[mid]==nums[end]){
+                start++;
                 end--;
                 continue;
             }
-            if(arr[str]<=arr[mid]){
-                if(arr[str]<=target && target<arr[mid]){
+            if(nums[start]<=nums[mid]){
+                if(nums[mid]>target && nums[start]<=target){
                     end=mid-1;
                 }
                 else{
-                    str=mid+1;
+                    start=mid+1;
                 }
             }
             else{
-                if(arr[mid]<target && target<=arr[end]){
-                    str=mid+1;
+                if(target>nums[mid] && target<=nums[end]){
+                    start=mid+1;
                 }
-                else{
-                    end=mid-1;
-                }
+                else end=mid-1;
             }
         }
         return false;
