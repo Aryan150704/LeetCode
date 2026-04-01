f073ed0a87b39f30c1135eeff4788ad6f3f257a9
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-04-01 11:17:25 +0530

    Time: 22 ms (5.36%) | Memory: 44.6 MB (45.25%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,39 +1,13 @@
 class Solution {
-    public String reverseWords(String st) {
-        ArrayList<String> arr=new ArrayList<String>();
-        StringBuilder stb=new StringBuilder();
-        int front=0;
-        int back=st.length()-1;
-        while(st.charAt(front)==' '){
-            front++;
+    public String reverseWords(String s) {
+        String[] words=s.trim().split("\\s+");
+        StringBuilder std=new StringBuilder();
+        for(int i=words.length-1;i>=0;i--){
+            System.out.println(words[i]);
+            std.append(words[i]);
+            if(i>0)
+            std.append(" ");
         }
-        while(st.charAt(back)==' '){
-            back--;
-        }
-        
-        String s=st.substring(front,back+1);
-        
-        for(int i=0;i<s.length();i++){
-           
-            if(s.charAt(i)==' '){
-                if(s.charAt(i-1)!=' '){
-                    arr.add(stb.toString());
-                stb.setLength(0);
-                }
-            }
-            else{
-                stb.append(s.charAt(i));
-            }
-        }
-        arr.add(stb.toString());
-        stb.setLength(0);
-        System.out.print(arr.size());
-        for(int i=arr.size()-1;i>=0;i--){
-            stb.append(arr.get(i));
-            if(i>0){ 
-                stb.append(" ");
-            }
-        }
-        return stb.toString();
+        return std.toString();
     }
 }
