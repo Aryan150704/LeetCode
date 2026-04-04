f2436c71775a2066d7c32a5d8ead98404b3072b2
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-04-04 09:15:07 +0530

    Time: 2 ms (2.06%) | Memory: 43.7 MB (92.86%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,13 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            int low=0;
-            int high=matrix[0].length-1;
-            while(low<=high){
-                int mid=(high+low)/2;
-                if(matrix[i][mid]==target) return true;
-                else if(matrix[i][mid]>target) high=mid-1;
-                else low=mid+1;
+        int left=0;
+        int right=matrix.length-1;
+        int mid=0;
+        int ansrow=right;
+        while(left<right){
+            mid=left+(right-left)/2;
+            if(matrix[mid][0]==target)return true;
+            if(matrix[mid][0]>target) right=mid-1;
+            else{
+                if(mid!=matrix.length-1 && matrix[mid+1][0]>target){
+                    ansrow=mid;
+                    break;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+        }
+        if(left==right){
+            ansrow=right;
+        }
+        System.out.println(ansrow);
+        left=0;
+        right=matrix[0].length-1;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(matrix[ansrow][mid]==target)return true;
+            if(matrix[ansrow][mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
         return false;
