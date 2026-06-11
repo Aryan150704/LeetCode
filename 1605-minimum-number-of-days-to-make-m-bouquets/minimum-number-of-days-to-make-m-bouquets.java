a67285f714e095161221503c2fbcc5dd3442b72a
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-06-11 17:22:06 +0530

    Time: 34 ms (5.94%) | Memory: 82.7 MB (26.20%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,40 +1,49 @@
 class Solution {
-    public boolean valid(int[] arr, int day, int m, int k) {
-    int bouquets = 0;
-    int flowers = 0;
-    for (int i = 0; i < arr.length; i++) {
-        if (arr[i] <= day) {
-            flowers++;
-                if (flowers == k) {
-                    bouquets++;
-                    flowers = 0;
-                }
-            } else {
-                flowers = 0;
-            }
-        }
-
-        return bouquets >= m;
-    }
-
     public int minDays(int[] bloomDay, int m, int k) {
+        // m -> bookey
+        // k -> flowers
+        if(m*k>bloomDay.length) return -1;
+
         int min=Integer.MAX_VALUE;
         int max=Integer.MIN_VALUE;
-        int minimumDays=-1;
+        int ans=-1;
         for(int i=0;i<bloomDay.length;i++){
             min=Math.min(min,bloomDay[i]);
             max=Math.max(max,bloomDay[i]);
         }
         while(min<=max){
             int mid=min+(max-min)/2;
-            if(valid(bloomDay,mid,m,k)){
+            if(checker(bloomDay,mid,m,k)){
+                ans=mid;
                 max=mid-1;
-                minimumDays=mid;
             }
-            else{
-                min=mid+1;
+            else min=mid+1;
+        }
+        /*
+        for(int i=min;i<=max;i++){
+            if(checker(bloomDay,i,m,k)){
+                ans=i;
+                break;
             }
         }
-        return minimumDays;
+        */
+        return ans;
+    }
+    public boolean checker(int[] arr,int n,int bookey,int flower){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            if(i==0 && arr[i]<=n)count++;
+            else if(flower==1 && arr[i]<=n)count++;
+            else if((i!=0)&&(arr[i-1] <= n && arr[i]<=n))count++;
+            else if((i!=0)&&(arr[i-1]>n && arr[i]<=n))count=1;
+            else count=0;
+            if(count==flower){
+                count=0;
+                bookey--;
+            }
+            
+        }
+        if(bookey<=0)return true;
+        return false;
     }
 }
