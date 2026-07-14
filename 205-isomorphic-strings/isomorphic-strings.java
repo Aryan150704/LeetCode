5745500082fce194aeb37055907b19cdca7efd41
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-07-14 16:07:22 +0530

    Time: 4 ms (99.33%) | Memory: 43.8 MB (74.25%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,28 +1,17 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length())return false;
-        HashMap<Character,Character> hm=new HashMap<>();
-        Boolean vise=false;
-        boolean versa=false;
-        for(int i=0;i<s.length();i++){
-            if(!hm.containsKey(s.charAt(i))){
-                hm.put(s.charAt(i),t.charAt(i));
-            }
-            else{
-                if(hm.get(s.charAt(i))!=t.charAt(i))return false;
-            }
+        int n=s.length();
+        int arr[]=new int[256];
+        int arr2[]=new int[256];
+        for(int i=0;i<n;i++){
+            char ch1=s.charAt(i);
+            char ch2=t.charAt(i);
+            
+            if(arr[ch1]!=arr2[ch2])return false;
+
+            arr[ch1]=i+1;
+            arr2[ch2]=i+1;
         }
-        vise=true;
-        hm.clear();
-        for(int i=0;i<t.length();i++){
-            if(!hm.containsKey(t.charAt(i))){
-                hm.put(t.charAt(i),s.charAt(i));
-            }
-            else{
-                if(hm.get(t.charAt(i))!=s.charAt(i))return false;
-            }
-        }
-        versa=true;
-        return vise && versa;
+        return true;
     }
 }
