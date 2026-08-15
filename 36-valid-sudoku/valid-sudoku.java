2a5dae16d854dc2f5a5b2be195c37d283da38908
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-08-15 22:05:55 +0530

    Time: 10 ms (24.05%) | Memory: 52.5 MB (9.80%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> seen =new HashSet<>();
+        Set<String> hs=new HashSet<>();
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                char num=board[i][j];
-                if(num!='.'){
-                    if(!seen.add(num+"in row"+i)||!seen.add(num+"in column"+j)||!seen.add(num+"at box"+i/3+"-"+j/3)) 
-                    return false;
-                }
+                if(board[i][j]=='.') continue;
+                if(!hs.add(board[i][j]+"in row"+i))return false;
+                if(!hs.add(board[i][j]+"in col"+j))return false;
+                if(!hs.add(board[i][j]+"in row and col"+i/3+j/3))return false;
+                // in case of !hs.add it will add then check so no need to add again
             }
         }
         return true;
