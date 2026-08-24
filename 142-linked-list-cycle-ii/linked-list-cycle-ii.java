0660021048caea42380109e4ce8a9fd75de1667e
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-08-24 16:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (50.23%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        boolean hasCycle=false;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                hasCycle=true;
+                break;
+            }
+        }
+        if(!hasCycle) return null;
+        ListNode num=head;
+        while(num!=slow){
+            num=num.next;
+            slow=slow.next;
+        }
+        return num;
+    }
+}
