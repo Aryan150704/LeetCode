4e6844dda7375ea7be8626e370ed51dc375767e4
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-08-25 17:57:41 +0530

    Time: 4 ms (99.72%) | Memory: 82.4 MB (80.49%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,43 +1,16 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergSort(nums,0,nums.length-1);
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int x : nums) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+        }
+        int[] count = new int[max - min + 1];
+        for (int x : nums) count[x - min]++;
+        int k = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i]-- > 0) nums[k++] = i + min;
+        }
         return nums;
-    }
-    public void mergSort(int[] arr,int si,int ei){
-        if(si>=ei){
-            return;
-        } 
-        int mid=si+(ei-si)/2;
-        mergSort(arr,si,mid); // left part
-        mergSort(arr,mid+1,ei); // right part
-        merge(arr,si,mid,ei);
-    }
-    public void merge(int[] arr,int si,int mid,int ei){
-        int i=si;
-        int j=mid+1;
-        int temp[]=new int[ei-si+1];
-        int k=0;
-        while(i<=mid && j<=ei){
-            if(arr[i]<arr[j]){
-                temp[k]=arr[i];
-                i++;
-            }
-            else{
-                temp[k]=arr[j];
-                j++;
-            }
-            k++;
-        }
-        while(i<=mid){
-            temp[k++]=arr[i++];
-        }
-        while(j<=ei){
-            temp[k++]=arr[j++];
-        }
-        int n=0;
-        for(int m=si;m<=ei;m++){
-            arr[m]=temp[n];
-            n++;
-        }
     }
 }
