436039af72d6e70b822cbc7a75b3d52f18b9db73
Aryan <160862593+Aryan150704@users.noreply.github.com>
2026-08-30 21:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (93.06%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,26 +14,19 @@
  * }
  */
 class Solution {
+    private void rightMost(TreeNode root,int level, List<Integer> ans){
+        if(root==null)return ;
+
+        if(ans.size()<level){
+            ans.add(root.val);
+        }
+
+        rightMost(root.right,level+1,ans);
+        rightMost(root.left,level+1,ans);
+    }
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
-        if(root==null)return ans;
-        Queue<TreeNode> que=new LinkedList<>();
-        que.offer(root);
-        while(!que.isEmpty()){
-            int height=que.size();
-            List<Integer> arr=new ArrayList<>();
-            for(int i=0;i<height;i++){
-                TreeNode curr=que.poll();
-                arr.add(curr.val);
-                if(curr.left!=null){
-                    que.offer(curr.left);
-                }
-                if(curr.right!=null){
-                    que.offer(curr.right);
-                }
-            }
-            ans.add(arr.get(arr.size()-1));
-        }
+        rightMost(root,1,ans);
         return ans;
     }
 }
